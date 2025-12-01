1e4920de204212540938bd0ab200d141857c1c5e
WazabiH <hamedwassila2004@gmail.com>
2025-12-01 14:10:32 +0100

    Changement_titre+correction erreur

--- code/server/api-router/message.js
+++ code/server/api-router/message.js
@@ -1,38 +1,81 @@
 import express from "express";
-import mongoose from "mongoose";
-
 import routeName from "#server/utils/name-route.middleware.js";
 import Message from "#models/message.js";
+import querystring from "querystring";
+import mongoose from "mongoose";
+
 
 const router = express.Router();
-
 const base = "messages";
 
-router.post(`/${base}`, routeName("message_api"), upload.single("image"), async (req, res) => {
+router.post(`/${base}`, routeName("message_api"), async (req, res) => {
     let listErrors = [];
 
-    const uploadedImage = req.body.file || req.file;
-
-    if (listErrors.length) {
-        return res.status(400).json({
-            errors: listErrors,
-            ressource: req.body,
-        });
-    }
-
     const ressource = new Message(req.body);
+
     try {
         await ressource.save();
-        res.status(201).json(ressourceComputed);
-    } catch (error) {
+        res.status(201).json(ressource);
+    } catch (err) {
         res.status(400).json({
             errors: [
                 ...listErrors,
-                ...deleteUpload(targetPath),
                 ...Object.values(
-                    error?.errors || [{ message: "Il y a eu un problème" }]
+                    err?.errors || [{ message: "Il y a eu un problème" }]
+                ).map(val => val.message),
+            ],
+            ressource: req.body,
+        });
+    }
+});
+router.get(`/${base}`, routeName("message_api_get_all"), async (req, res) => {
+    try {
+        const messages = await Message.find();
+        console.log(messages);
+        res.json(messages); // la liste avec les messages
+    } catch (err) {
+        res.status(500).json({ error: "Erreur" }); // si il y a une erreur coté server
+    }
+});
+
+
+
+// get messages
+router.get(`/${base}`, routeName("message_api_get_all"), async (req, res) => {
+    const page = Math.max(1, Number(req.query.page) || 1);
+    let perPage = Number(req.query.per_page) || 7;
+    // Clamps the value between 1 and 20
+    perPage = Math.min(Math.max(perPage, 1), 20);
+
+    try {
+        const listRessources = await Message.aggregate([
+            { $sort: { _id: -1 } },
+            { $skip: Math.max(page - 1, 0) * perPage },
+            { $limit: perPage },
+        ]);
+
+        const count = await Message.countDocuments(
+        );
+
+        const queryParam = { ...req.query };
+        delete queryParam.page;
+
+        res.status(200).json({
+            data: listRessources,
+            total_pages: Math.ceil(count / perPage),
+            count,
+            page,
+            query_params: querystring.stringify(queryParam),
+        });
+    } catch (e) {
+        res.status(400).json({
+            errors: [
+                ...Object.values(
+                    e?.errors || [{ message: e?.message || "Il y a eu un problème" }]
                 ).map(val => val.message),
             ],
         });
     }
 });
+
+export default router;
